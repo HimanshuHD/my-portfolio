51f926451499bc75bb5650b4fdd23f5f00d72d4a
Himanshu Digari <himanshudigari@gmail.com>
2026-08-08 23:11:27 +0530

    feat: add route page transitions

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,7 +1,8 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import PageTransition from '../components/PageTransition';
 
 export default function MainLayout() {
-  return <div className="site"><Header /><main><Outlet /></main><Footer /></div>;
+  return <div className="site"><Header /><main><PageTransition><Outlet /></PageTransition></main><Footer /></div>;
 }
