17c333b81645a9014e38692c26df9b0984faae5b
Himanshu Digari <himanshudigari@gmail.com>
2026-08-08 23:11:19 +0530

    feat: add page transition wrapper

---
+++ src/components/PageTransition.jsx
@@ -0,0 +1,6 @@
+import { useLocation } from 'react-router-dom';
+
+export default function PageTransition({ children }) {
+  const location = useLocation();
+  return <div className="page-transition" key={location.pathname}>{children}</div>;
+}
