773c2cff876db914e6b097a83a000e13fc9a00a3
Himanshu Digari <himanshudigari@gmail.com>
2026-08-11 23:54:06 +0530

    fix(rubiks): use actual 3D cube as game trigger
    Refs #11

--- src/components/home/InteractiveScene/InteractiveScene.jsx
+++ src/components/home/InteractiveScene/InteractiveScene.jsx
@@ -1,17 +1,7 @@
 import LegacyInteractiveScene from '../InteractiveScene';
-import RubiksCube from './RubiksCube';
 
 export default function InteractiveScene({ onCubeClick }) {
   return <div className="interactive-scene-modular">
-    <LegacyInteractiveScene />
-    <button className="rubiks-scene-hit-target" type="button" aria-label="Open Rubik's Cube game" onClick={onCubeClick}>
-      <RubiksCubePreview />
-    </button>
+    <LegacyInteractiveScene onCubeClick={onCubeClick} />
   </div>;
 }
-
-function RubiksCubePreview() {
-  return <span className="rubiks-scene-hit-cube" aria-hidden="true" />;
-}
-
-export { RubiksCube };
